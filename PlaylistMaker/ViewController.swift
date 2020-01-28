3a75c09947fa37237316238038361fc3b60346fd
Christopher Boynton <cboynton16@gmail.com>
2020-01-28 07:51:34 -0500

    initial commit - new project from Xcode

---
+++ PlaylistMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlaylistMaker
+//
+//  Created by Christopher Boynton on 1/28/20.
+//  Copyright © 2020 Christopher Boynton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
