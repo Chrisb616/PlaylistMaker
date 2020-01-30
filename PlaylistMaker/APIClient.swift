5314559169ce2731662081d1a42a420d17055b5a
Christopher Boynton <cboynton16@gmail.com>
2020-01-30 10:00:12 -0500

    APIClient now successfully pulling all tracks for a date section recursively

--- PlaylistMaker/APIClient.swift
+++ PlaylistMaker/APIClient.swift
@@ -18,7 +18,7 @@ class APIClient {
     let recentTracksMethod = "user.getrecenttracks"
     
     private func urlRequest(httpMethod: String, apiMethod: String, params: [String:String]) -> URLRequest? {
-        guard var url = URL(string: lastFmUrl) else {
+        guard let url = URL(string: lastFmUrl) else {
             print("FAILURE: Could not create URL from base url string")
             return nil
         }
@@ -29,6 +29,8 @@ class APIClient {
             paramString += "&\(param.key)=\(param.value)"
         }
         
+        print("Request Endpoint: \(url.absoluteString + paramString)")
+        
         var request = URLRequest(url: url)
         request.httpMethod = httpMethod
         request.httpBody = paramString.data(using: .utf8)
@@ -37,50 +39,31 @@ class APIClient {
     }
     
     
-    func getRecentTracksfrom(_ startDate: Date, to endDate: Date, completion: @escaping (RecentTracksResponse?,String)->()) {
-        
+    func getRecentTracksfrom(_ startDate: Date, to endDate: Date, completion: @escaping ([RecentTracksResponse],String?)->()) {
+        getRecentTracksRecursive(startDate, to: endDate, currentPage: 1, progress: []) { (responses, errorString) in
+            completion(responses,errorString)
+        }
+    }
+    
+    /**
+     Completes a data task for the request. Completion handles response data, error string.
+     */
+    private func getRecentTracksRecursive(_ startDate: Date, to endDate: Date, currentPage: Int, progress: [RecentTracksResponse], completion: @escaping ([RecentTracksResponse],String?)->()) {
         let params = [
             "user":"chrisb616",
             "api_key":Keys.apiKey,
             "format":"json",
             "limit":"200",
-            "from":"\(startDate.timeIntervalSince1970)",
-            "to":"\(endDate.timeIntervalSince1970)"
+            "from":"\(Int(startDate.timeIntervalSince1970))",
+            "to":"\(Int(endDate.timeIntervalSince1970))",
+            "page":"\(currentPage)"
         ]
         
         guard let request = urlRequest(httpMethod: "POST", apiMethod: recentTracksMethod, params: params) else {
-            completion(nil, "FAILURE: Could not create url request.")
+            completion(progress, "FAILURE: Could not create url request for page 1.")
             return
         }
         
-        paginatedRequest(request: request) { (data, errorString, success) in
-            guard let data = data else {
-                if let errorString = errorString {
-                    completion(nil, errorString)
-                } else {
-                    completion(nil, "FAILURE: Could not retrieve data from URL.")
-                }
-                return
-            }
-            
-            do {
-                
-                let recentTracksResponse = try JSONDecoder().decode(RecentTracksResponse.self, from: data)
-                let string = String(data: data, encoding: .utf8)
-                let dict = try JSONSerialization.jsonObject(with: data, options: []) as! [String:[String:Any]]
-                let recentTracks = dict["recenttracks"]!
-                let attr = recentTracks["@attr"] as! [String:Any]
-                print(attr)
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
-    /**
-     Completes a data task for the request. Completion handles response data, error string, and a boolean for success.
-     */
-    private func paginatedRequest(request: URLRequest, completion: @escaping (Data?,String?,Bool)->()) {
         let session = URLSession.shared
         
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -90,11 +73,59 @@ class APIClient {
             }
             
             guard let data = data else {
-                completion(nil, "FAILURE: Could not retrieve data from URL.", true)
+                completion(progress, "FAILURE: Could not retrieve data from URL.")
                 return
             }
             
-            completion(data, nil, true)
+            do {
+                
+                let recentTracksResponse = try JSONDecoder().decode(RecentTracksResponse.self, from: data)
+                var newProgress = [RecentTracksResponse]()
+                newProgress.append(recentTracksResponse)
+                
+                guard let dict = try JSONSerialization.jsonObject(with: data, options: []) as? [String:[String:Any]] else {
+                    completion(progress,"Could not convert JSON into dictionary")
+                    return
+                }
+                
+                guard let recentTracks = dict["recenttracks"] else {
+                    completion(progress,"No tag 'recenttracks' inside of dictionary object")
+                    return
+                }
+                
+                guard let attr = recentTracks["@attr"] as? [String:String] else {
+                    completion(progress,"No tag '@attr' inside of recenttracks dictionary")
+                    return
+                }
+                
+                guard let totalPagesObject = attr["totalPages"] else {
+                    completion(progress,"No tag 'totalPages' inside of attr dictionary")
+                    return
+                }
+                
+                guard let totalPages = Int(totalPagesObject) else {
+                    completion(progress,"Object 'totalPages' cannot be cast to Int")
+                    return
+                }
+                
+                if (totalPages == 0) {
+                    completion(progress,"No tracks found for date")
+                    return
+                }
+                
+                print("\(currentPage)/\(totalPages)")
+                
+                if (totalPages == currentPage) {
+                    completion(newProgress, nil)
+                } else {
+                    self.getRecentTracksRecursive(startDate, to: endDate, currentPage: currentPage + 1, progress: newProgress) { (finalProgress, nil) in
+                        completion(newProgress, nil)
+                    }
+                }
+                
+            } catch {
+                completion(progress, error.localizedDescription)
+            }
         }
         
         task.resume()
