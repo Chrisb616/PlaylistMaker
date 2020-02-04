4e1116f9f1472a2b199ba2a659ee7181c4ced262
Christopher Boynton <cboynton16@gmail.com>
2020-02-04 12:53:11 -0500

    Progress on the Playlist table view

---
+++ PlaylistMaker/ViewControllers/UserMenuPlaylistTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserMenuPlaylistTableViewCell.swift
+//  PlaylistMaker
+//
+//  Created by Christopher Boynton on 2/4/20.
+//  Copyright © 2020 Christopher Boynton. All rights reserved.
+//
+
+import UIKit
+
+class UserMenuPlaylistTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
